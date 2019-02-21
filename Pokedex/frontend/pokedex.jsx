9d3b4586473cd953058302576aa033081a1c0df7
ioqren <corriestoddard@gmail.com>
2019-02-20 17:02:28 -0800

    Update pokedex app

--- Pokedex/frontend/pokedex.jsx
+++ Pokedex/frontend/pokedex.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom';
 import { fetchAllPokemon } from "./util/api_util"
 import { receiveAllPokemon } from "./actions/pokemon_actions"
+import pokemonReducer from "./reducers/pokemon_reducer"
 
 document.addEventListener('DOMContentLoaded', () => {
   const rootEl = document.getElementById('root');
   window.fetchAllPokemon = fetchAllPokemon;
   window.receiveAllPokemon = receiveAllPokemon;
+  window.pokemonReducer = pokemonReducer;
   ReactDOM.render(<h1>Pokedex</h1>, rootEl);
 });
  
--- Pokedex/frontend/reducers/pokemon_reducer.js
+++ Pokedex/frontend/reducers/pokemon_reducer.js
@@ -1,13 +1,15 @@
 import { RECEIVE_ALL_POKEMON } from '../actions/pokemon_actions';
 import { fetchAllPokemon } from '../util/api_util'
+import {merge} from 'lodash'
 
 const pokemonReducer = (state, action) => {
   let nextState = {}
   Object.freeze(state);
   switch(action.type) {
     case RECEIVE_ALL_POKEMON:
-      nextState = fetchAllPokemon();
-      nextState = nextState.responseJSON;
+      let pokemon = setTimeout(fetchAllPokemon(), 2000);
+      console.log(pokemon.responseJSON);
+      nextState = merge({}, state, pokemon.responseJSON);
       return nextState;
     default:
       return state;
